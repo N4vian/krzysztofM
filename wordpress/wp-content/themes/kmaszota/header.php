168680d5d62130ab23f37b974f959677618be390
Lukasz Sadurski <lukaszsadurski@gmail.com>
2019-08-07 12:21:40 +0200

    BFC - wordrpess start

---
+++ wordpress/wp-content/themes/kmaszota/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>Krzysztof Maszota</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" />
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" />
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet" />
+  <!-- Your custom styles (optional) -->
+  <link href="css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+  <!-- Start your project here-->
+  <!--Main Navigation-->
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark scrolling-navbar gradient-darkblue-blue">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse navbar--container" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">O mnie <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Darmowe narzędzia</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kursy online</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+        </ul>
+        <a class="navbar-brand" href="#"><img src="img/logo.png" alt="" /></a>
+      </div>
+    </nav>
+  </header>
+  <!--Main Navigation-->
